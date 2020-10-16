c29bf0a65eb9b2a20a8fd9f25ebe227fe3262bd1
cathe-zhang <zhangle_media@hotmail.com>
2020-10-16 03:46:30 +0000

    deploy

---
+++ precache-manifest.f57784ef1fe4b65dce326b03a9049e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037e0d803cd3c39d04375f76230b0a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4619dc0e43e9e1186d41",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "433aaf995f7b6f79c18d",
+    "url": "/static/js/2.90b295f5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.90b295f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4619dc0e43e9e1186d41",
+    "url": "/static/js/main.1bc6bfb4.chunk.js"
+  },
+  {
+    "revision": "3cb1b205d1841ede3a98",
+    "url": "/static/js/runtime-main.e763f6c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
